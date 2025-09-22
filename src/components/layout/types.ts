e87719424161c3d6dc731541f08503f4209f8fe1
jin <jinsu.kim1011@gmail.com>
2025-09-22 10:09:41 +0200

    feat: add NavItem type for navigation item structure

---
+++ src/components/layout/types.ts
@@ -0,0 +1,8 @@
+export type NavItem = {
+    to: string;
+    label: string;
+    icon?: React.ReactNode;
+    disabled?: boolean; // for the feature that are not avaialbe yet
+    external?: boolean; // wheter the link is external link
+    target?: "_blank" | "_self"; // for external link targert, opens in new tab or same tab
+};
