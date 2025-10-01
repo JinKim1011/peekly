ccfbfcceed6c7c09af019cadb69b9126bb5a6b8e
jin <jinsu.kim1011@gmail.com>
2025-10-01 16:13:02 +0200

    feat: add State component to display loading and error states in Contacts

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -6,6 +6,7 @@ import { designTokens } from '../design-tokens';
 import { ListPaneStyle } from '../styles/ListPane';
 import { PageStyle } from "../styles/Page";
 import { TitleWrapper } from "../components/common/TitleWrapper";
+import { State } from "../components/common/State";
 
 
 
@@ -24,6 +25,13 @@ export default function Contacts() {
     display: 'flex',
     padding: `${designTokens.spacing.s} ${designTokens.spacing.l} ${designTokens.spacing.l}`,
   }
+
+  const stateText =
+    error ? 'Something went wrong. Please try again later'
+      : loading ? 'Please wait...'
+        : !selectedId ? 'No customer selected' : '';
+
+  const selectedContact = contactsWithContact.find(contact => contact.id === selectedId);
 
   return (
     <div style={PageStyle}>
@@ -52,6 +60,29 @@ export default function Contacts() {
           loading={loading}
           error={error}
         />
+      </div>
+      <div style={{
+        maxWidth: 400,
+        margin: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        flex: `1 0 0`,
+        padding: `0 ${designTokens.spacing.l}`,
+      }}>
+        {error ? (
+          <State variant="error" title={stateText} />
+        ) : loading ? (
+          <State variant="loading" title={stateText} />
+        ) : selectedContact ? (
+          <pre style={{
+            ...designTokens.typography.micro,
+            whiteSpace: 'pre-wrap',
+          }}>
+            {JSON.stringify(selectedContact, null, 2)}
+          </pre>
+        ) : (
+          <State variant="empty" title={stateText} />
+        )}
       </div>
     </div>
   );
