497b75ebf3fd261ea0cb0a702caffef24a8f89fd
jin <jinsu.kim1011@gmail.com>
2025-10-01 17:25:07 +0200

    feat: start the worker in development before React renders

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,27 @@ import { store } from "./store/index.ts";
 import App from "./App.tsx";
 import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
-);
+async function enableMocks() {
+  if (import.meta.env.MODE! === 'development') return;
+  const { worker } = await import('./mocks/browser');
+
+  await worker.start({
+    serviceWorker: {
+      url: '/mockServiceWorker.js',
+    },
+    onUnhandledRequest: 'bypass'
+  });
+}
+
+enableMocks().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  );
+});
+
